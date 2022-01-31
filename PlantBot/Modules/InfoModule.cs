9896905c6fbdece42ccfa65697a58631824d968c
JScrivo <scrivjor@sheridancollege.ca>
2022-01-30 20:55:21 -0500

    Implemented command to take picture of plant and send it to server

--- PlantBot/Modules/InfoModule.cs
+++ PlantBot/Modules/InfoModule.cs
@@ -1,4 +1,5 @@
 ﻿using Discord.Commands;
+using PlantBot.Pi;
 
 namespace PlantBot.Modules
 {
@@ -8,6 +9,20 @@ namespace PlantBot.Modules
         public async Task PingAsync()
         {
             await ReplyAsync("Pong");
+        }
+
+        [Command("cam")]
+        public async Task CameraAsync()
+        {
+            Camera camera = new Camera();
+
+            await camera.TakePicture();
+
+            var directory = new DirectoryInfo("/home/pi/images");
+
+            var file = directory.GetFiles().OrderByDescending(f => f.LastWriteTime).First();
+
+            await Context.Channel.SendFileAsync(file.FullName);
         }
     }
 }
---
+++ PlantBot/Pi/Camera.cs
@@ -0,0 +1,29 @@
+﻿using MMALSharp;
+using MMALSharp.Common;
+using MMALSharp.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantBot.Pi
+{
+    public class Camera
+    {
+        public async Task TakePicture()
+        {
+            // Singleton initialized lazily. Reference once in your application.
+            MMALCamera cam = MMALCamera.Instance;
+
+            using (var imgCaptureHandler = new ImageStreamCaptureHandler("/home/pi/images/", "jpg"))
+            {
+                await cam.TakePicture(imgCaptureHandler, MMALEncoding.JPEG, MMALEncoding.I420);
+            }
+
+            // Cleanup disposes all unmanaged resources and unloads Broadcom library. To be called when no more processing is to be done
+            // on the camera.
+            cam.Cleanup();
+        }
+    }
+}
